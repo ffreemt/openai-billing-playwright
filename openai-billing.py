39d1c0f0ddc6e338388bd5aa577fefdae8472d8c
ffreemt <yucongo+fmt@gmail.com>
2023-09-05 11:24:24 +0800

    Update rh-release.yml: defaults: run: shell: bash/set PLAYWRIGHT_BROWSERS_PATH=0; openai-billing.py: os.environ['PLAYWRIGHT_BROWSERS_PATH'] = '0'

--- openai-billing.py
+++ openai-billing.py
@@ -1,12 +1,15 @@
-"""Prep for nuitka build."""
-# ruff: noqa: F401
-# import gradio_client  # nuitka
-# import loguru
-# import pyperclip
-# import rich
-# import typer
+"""
+Prep for nuitka build.
 
+# https://github.com/Nuitka/Nuitka/issues/1029
+set PLAYWRIGHT_BROWSERS_PATH=0
+python -m playwright install chromium
+"""
+import os
 from openai_billing.__main__ import app
+
+# https://github.com/Nuitka/Nuitka/issues/1029
+os.environ['PLAYWRIGHT_BROWSERS_PATH'] = '0'
 
 if __name__ == "__main__":
     app()
