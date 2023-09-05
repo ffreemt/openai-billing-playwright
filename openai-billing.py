e698a03854264ddc280c87cc47e8793e164dd702
ffreemt <yucongo+fmt@gmail.com>
2023-09-05 11:25:32 +0800

    Update rh-release.yml: defaults: run: shell: bash/set PLAYWRIGHT_BROWSERS_PATH=0; openai-billing.py: os.environ['PLAYWRIGHT_BROWSERS_PATH'] = '0'

--- openai-billing.py
+++ openai-billing.py
@@ -6,10 +6,11 @@ set PLAYWRIGHT_BROWSERS_PATH=0
 python -m playwright install chromium
 """
 import os
+
 from openai_billing.__main__ import app
 
 # https://github.com/Nuitka/Nuitka/issues/1029
-os.environ['PLAYWRIGHT_BROWSERS_PATH'] = '0'
+os.environ["PLAYWRIGHT_BROWSERS_PATH"] = "0"
 
 if __name__ == "__main__":
     app()
